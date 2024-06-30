1352950d8b4f99afa710568967b811fde0101586
HangNT <HangNT169@fpt.edu.vn>
2024-07-01 00:07:02 +0700

    init project

---
+++ src/su24_xuong_desktop_nhom2/SU24_Xuong_Desktop_Nhom2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package su24_xuong_desktop_nhom2;
+
+/**
+ *
+ * @author hangnt
+ */
+public class SU24_Xuong_Desktop_Nhom2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
